cc62d72c8a8219a5430738a935d83b5872604d16
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2018-08-03 22:05:33 +0300

    Paransin selitystä tyhjästä primer-avaimesta autokey salauksessa

--- CryptoApp/src/main/java/crypto/ciphers/AutokeyVigenereCipher.java
+++ CryptoApp/src/main/java/crypto/ciphers/AutokeyVigenereCipher.java
@@ -30,7 +30,8 @@ public class AutokeyVigenereCipher extends VigenereCipher {
      *
      * @param primer A normal encryption key that is only used once and is not
      * lengthened unlike in the standard Vigenere cipher. The key should only
-     * contain the standard 26 Latin alphabet characters.
+     * contain the standard 26 Latin alphabet characters and should not be
+     * empty, although this is not enforced in any way.
      * @param plaintext The message to be encrypted. The message should only
      * contain the standard 26 Latin alphabet characters.
      * @return The original message in encrypted form
@@ -48,10 +49,12 @@ public class AutokeyVigenereCipher extends VigenereCipher {
      * message can then be used to repeat the decryption and this whole process
      * is repeated until the whole ciphertext is decrypted.
      *
-     * An empty primer makes it impossible to decrypt the message since no
-     * information about the key (which was the original plaintext) can be
-     * gained by using the ciphertext only. Therefore the ciphertext is returned
-     * (which is not the original plaintext).
+     * An empty primer would make the simple decryption process into a
+     * cryptanalytic process of guessing the plaintext since for instance both b
+     * encrypted with b and o encrypted with o produce c as the output. Hence,
+     * no such cryptanalysis is performed, since decryption should be a
+     * straightforward deterministic process, and therefore the ciphertext is
+     * returned (which is not the original plaintext).
      *
      * @param primer A normal encryption key that is only used once and is not
      * lengthened unlike in the standard Vigenere cipher. The key should only
@@ -60,10 +63,10 @@ public class AutokeyVigenereCipher extends VigenereCipher {
      * plaintext and this makes it impossible to decrypt the key since the
      * original plaintext should be known but encrypting a plaintext using the
      * exactly same plaintext as the key produces a different ciphertext and
-     * since the primer is empty the plaintext cannot be known. Therefore an
-     * empty primer returns the ciphertext (which is the wrong answer) and thus
-     * an empty primer should not be provided since then all information of the
-     * encryption key is lost.
+     * since the primer is empty the plaintext cannot be known
+     * deterministically. Therefore an empty primer returns the ciphertext
+     * (which is the wrong answer) and thus an empty primer should not be
+     * provided.
      * @param ciphertext The message to be decrypted. The message should only
      * contain the standard 26 Latin alphabet characters.
      * @return The original message in plaintext
