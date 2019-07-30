51795ce6aa6b86bfb5c0f3cde8bf71d361560855
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-07-31 00:56:19 +0300

    Remove unused import

--- CryptoApp/src/main/java/crypto/cryptoapp/MenuView.java
+++ CryptoApp/src/main/java/crypto/cryptoapp/MenuView.java
@@ -5,7 +5,6 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
